5e91ee854e970de58a972bb868ed72faaf505ead
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-03 13:28:45 +0200

    toast added

--- src/components/ChefRecipes/ChefRecipes.jsx
+++ src/components/ChefRecipes/ChefRecipes.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { FaBeer, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaBeer, FaHeart, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const ChefRecipes = ({recipe}) => {
-    const {name,img,ingredients,rating} = recipe
+    const {name,img,ingredients,rating} = recipe;
+    const [isFavorite, setIsFavorite] = useState(false);
+    const handleFavoriteClick = () => {
+      setIsFavorite(true);
+      toast('Recipe added to favorites!');
+    };
+    
     return (
+
         <div >
+          <ToastContainer />
             <div className="card  bg-base-100 shadow-xl">
   <figure className="px-10 pt-10">
     <img  src={img} alt="Shoes" className="rounded-xl h-96 w-96" />
@@ -14,9 +24,10 @@ const ChefRecipes = ({recipe}) => {
     <h2 className="card-title">{name}</h2>
     <p>Ingredients: {ingredients}</p>
     <div className='flex items-center'> <FaStar/><FaStar/><FaStar/><FaStar/> <FaStarHalfAlt/> <span>{rating}</span> </div>
-    <div className="card-actions">
+    <div className="card-actions flex">
       
       <Link to={`/fullRecipe/${recipe.recipe_id}`} > <button className="btn btn-primary">Show Full Recipe</button></Link>
+      <button className="btn btn-primary" onClick={handleFavoriteClick} disabled={isFavorite}> <FaHeart className='text-white hover:text-red-500'></FaHeart></button>
     </div>
   </div>
 </div>
