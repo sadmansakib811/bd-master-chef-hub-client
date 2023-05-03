87a629711fef8989d7027df03235f65e9e06efa1
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-03 11:30:44 +0200

    route dynamic param link creation done

--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
         element: <Register></Register>
       },
       {
-        path: '/chef/:',
+        path: '/chef/:id',
         element: <Chef></Chef>,
         loader:({params})=>fetch(`http://localhost:5000/chef/${params.id}`)
         
